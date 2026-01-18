48810eda867ee43fd593ff1c74d9d9c34683d695
maxxxxx2603 <powerr.proooo@gmail.com>
2026-01-18 12:15:11 +0100

    Ajout de la commande /update pour afficher les statistiques des customs par employé

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ChannelType, PermissionFlagsBits, AttachmentBuilder } = require('discord.js');
+﻿const { Client, GatewayIntentBits, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ChannelType, PermissionFlagsBits, AttachmentBuilder } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
 require('dotenv').config();
